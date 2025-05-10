804e7b10c44fcc4f51775d0e506df50b01216230
syahrilhanla <syahrilhanla5@gmail.com>
2025-05-10 17:01:57 +0700

    feat: add formatCountDate function to display time since a given date

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -16,3 +16,41 @@ export const formatThousandsToK = (value: number): string => {
   }).format(value);
   return formattedValue;
 }
+
+export const formatCountDate = ((date: string) => {
+  const now = new Date();
+  const reviewDate = new Date(date);
+  const diffInSeconds = Math.floor(
+    (now.getTime() - reviewDate.getTime()) / 1000
+  );
+
+  const secondsInMinute = 60;
+  const secondsInHour = 60 * secondsInMinute;
+  const secondsInDay = 24 * secondsInHour;
+  const secondsInMonth = 30 * secondsInDay;
+  const secondsInYear = 12 * secondsInMonth;
+
+  if (diffInSeconds < secondsInMinute) {
+    return `${diffInSeconds} seconds ago`;
+  } else if (diffInSeconds < secondsInHour) {
+    return `${Math.floor(
+      diffInSeconds / secondsInMinute
+    )} minutes ago`;
+  } else if (diffInSeconds < secondsInDay) {
+    return `${Math.floor(
+      diffInSeconds / secondsInHour
+    )} hours ago`;
+  } else if (diffInSeconds < secondsInMonth) {
+    return `${Math.floor(
+      diffInSeconds / secondsInDay
+    )} days ago`;
+  } else if (diffInSeconds < secondsInYear) {
+    return `${Math.floor(
+      diffInSeconds / secondsInMonth
+    )} months ago`;
+  } else {
+    return `${Math.floor(
+      diffInSeconds / secondsInYear
+    )} years ago`;
+  }
+})
