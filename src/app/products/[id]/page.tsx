eb0527ebccdfaac389ecf7423b785d3fb365ed97
syahrilhanla <syahrilhanla5@gmail.com>
2025-05-10 17:13:13 +0700

    feat: extract main info and review section to new component

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,14 +1,10 @@
 "use client";
 
+import ProductMainInfo from "@/components/ProductDetail/ProductMainInfo";
+import ProductReview from "@/components/ProductDetail/ProductReview";
 import ProductDetailSkeleton from "@/components/ProductDetailSkeleton";
-import {
-	formatCountDate,
-	formatPriceIDR,
-	formatThousandsToK,
-} from "@/helpers/helpers";
 import { Product } from "@/types/Product.type";
-import { BuildingStorefrontIcon, StarIcon } from "@heroicons/react/16/solid";
-import { Button, Image } from "@heroui/react";
+import { Image } from "@heroui/react";
 import { useQuery } from "@tanstack/react-query";
 import { use } from "react";
 
@@ -69,138 +65,13 @@ const ProductDetail = ({ params }: Props) => {
 						</div>
 
 						{/* main info */}
-						<div className="flex flex-col gap-2">
-							<h2 className="text-gray-200 text-xl font-semibold">
-								{product.title}
-							</h2>
-							<div className="flex items-center gap-1 text-gray-400 text-sm">
-								+
-								<span className="text-gray-200 font-semibold">
-									{formatThousandsToK(
-										Number((Math.random() * 10000).toFixed(0))
-									)}
-								</span>{" "}
-								sold
-								<small className="text-gray-400 px-2 text-base">•</small>
-								<StarIcon width={16} color="yellow" />
-								<small className="text-sm text-gray-200 font-semibold">
-									{product.rating}
-								</small>
-							</div>
-							<h4 className="text-gray-100 text-3xl font-semibold">
-								{formatPriceIDR(
-									(product.price + product.discountPercentage) * 1000
-								)}
-							</h4>
-							<hr className="border-gray-800 border-t my-2" />
-
-							<div className="pb-2 grid gap-1.5">
-								<div className="text-sm">
-									<span className="text-gray-400">Condition:</span>{" "}
-									<span className="font-semibold text-gray-100">New</span>
-								</div>
-								<div className="text-sm">
-									<span className="text-gray-400">Min Order Qty:</span>{" "}
-									<span className="font-semibold text-gray-100">
-										{product.minimumOrderQuantity}
-									</span>
-								</div>{" "}
-								<div className="text-sm">
-									<span className="text-gray-400">Warranty:</span>{" "}
-									<span className="font-semibold text-gray-100">
-										{product.warrantyInformation}
-									</span>
-								</div>
-								<div className="text-sm">
-									<span className="text-gray-400">Return Policy:</span>{" "}
-									<span className="font-semibold text-gray-100">
-										{product.returnPolicy}
-									</span>
-								</div>
-							</div>
-
-							<div className="text-sm text-gray-200">{product.description}</div>
-
-							<hr className="border-gray-800 border-t my-2" />
-
-							<div className="w-full flex items-center text-base justify-between">
-								<span className="flex gap-2">
-									<BuildingStorefrontIcon width={14} />
-									<p className="text-base font-bold text-gray-100">
-										{product.brand}
-									</p>
-								</span>
-								<Button
-									className="text-gray-200 text-sm font-semibold bg-gray-800 hover:bg-gray-700"
-									variant="solid"
-									color="danger"
-									size="sm"
-								>
-									Follow{" "}
-								</Button>
-							</div>
-
-							<hr className="border-gray-800 border-t my-2" />
-
-							<div className="flex justify-between text-sm text-gray-400">
-								Problem with this product?
-								<p>
-									⚠{" "}
-									<span className="text-gray-100 cursor-pointer font-semibold">
-										{" "}
-										Report
-									</span>
-								</p>
-							</div>
-						</div>
+						<ProductMainInfo product={product} />
 
 						{/* cart and payment info */}
 						<div></div>
 					</div>
 
-					<aside className="w-full px-4">
-						<h5 className="text-gray-200 text-2xl">Reviews</h5>
-
-						<div className="mt-4 space-y-4">
-							{product.reviews.length > 0 ? (
-								product.reviews.map((review, index) => (
-									<div
-										key={index}
-										className={`p-4 ${
-											index !== product.reviews.length - 1
-												? "border-b border-gray-800"
-												: ""
-										} bg-transparent`}
-									>
-										<div className="">
-											<div className="flex gap-2">
-												<span className="flex items-center gap-1">
-													{Array.from({ length: 5 }, (_, i) => (
-														<StarIcon
-															key={i}
-															width={16}
-															color={i < review.rating ? "orange" : "gray"}
-														/>
-													))}
-												</span>
-												<p>{formatCountDate(review.date)}</p>
-											</div>
-											<h6 className="text-gray-200 font-semibold">
-												{review.reviewerName}
-											</h6>
-										</div>
-										<p className="text-sm text-gray-300 mt-2">
-											{review.comment}
-										</p>
-									</div>
-								))
-							) : (
-								<p className="text-gray-400">
-									No reviews available for this product.
-								</p>
-							)}
-						</div>
-					</aside>
+					<ProductReview product={product} />
 				</div>
 			)}
 		</>
---
+++ src/components/ProductDetail/ProductReview.tsx
@@ -0,0 +1,55 @@
+import { formatCountDate } from "@/helpers/helpers";
+import { Product } from "@/types/Product.type";
+import { StarIcon } from "@heroicons/react/16/solid";
+
+interface Props {
+	product: Product;
+}
+
+const ProductReview = ({ product }: Props) => {
+	return (
+		<aside className="w-full px-4">
+			<h5 className="text-gray-200 text-2xl">Reviews</h5>
+
+			<div className="mt-4 space-y-4">
+				{product.reviews.length > 0 ? (
+					product.reviews.map((review, index) => (
+						<div
+							key={index}
+							className={`p-4 ${
+								index !== product.reviews.length - 1
+									? "border-b border-gray-800"
+									: ""
+							} bg-transparent`}
+						>
+							<div className="">
+								<div className="flex gap-2">
+									<span className="flex items-center gap-1">
+										{Array.from({ length: 5 }, (_, i) => (
+											<StarIcon
+												key={i}
+												width={16}
+												color={i < review.rating ? "orange" : "gray"}
+											/>
+										))}
+									</span>
+									<p>{formatCountDate(review.date)}</p>
+								</div>
+								<h6 className="text-gray-200 font-semibold">
+									{review.reviewerName}
+								</h6>
+							</div>
+							<p className="text-sm text-gray-300 mt-2">{review.comment}</p>
+						</div>
+					))
+				) : (
+					<p className="text-gray-400">
+						No reviews available for this product.
+					</p>
+				)}
+			</div>
+		</aside>
+	);
+};
+
+export default ProductReview;
