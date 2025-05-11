0415f0d413e6659bf705ca1192c019cbd6631c3d
syahrilhanla <syahrilhanla5@gmail.com>
2025-05-11 23:21:48 +0700

    feat: enhance addProduct logic to update existing cart items and calculate totals

--- src/store/cart.store.ts
+++ src/store/cart.store.ts
@@ -11,10 +11,33 @@ interface CartState {
   clearCart: () => void;
 }
 
-export const useCartStore = create<CartState>((set) => ({
+export const useCartStore = create<CartState>((set, get) => ({
   products: [],
   total: 0,
   addProduct: (product) => {
+    const existingProduct = get().products.find((p) => p.id === product.id);
+
+    // If the product already exists in the cart, update its quantity
+    if (existingProduct) {
+      const updatedProducts = get().products.map((p) =>
+        p.id === product.id
+          ? {
+            ...p,
+            quantity: p.quantity + product.quantity,
+            total: p.total + product.price * product.quantity,
+            discountedTotal: p.discountedTotal + (product.price * product.quantity - (product.price * product.discountPercentage) / 100)
+          }
+          : p
+      );
+      const total = updatedProducts.reduce(
+        (sum, p) => sum + p.discountedTotal,
+        0
+      );
+
+      return set({ products: updatedProducts, total });
+    }
+
+    // If the product does not exist, add it to the cart
     const discountedTotal = product.price * product.quantity - (product.price * product.discountPercentage) / 100;
     const total = product.price * product.quantity;
 
