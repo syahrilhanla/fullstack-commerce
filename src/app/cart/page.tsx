892d6a24e415ddc84008b8ee307bc5aff3eacd0c
syahrilhanla <syahrilhanla5@gmail.com>
2025-05-12 09:32:36 +0700

    feat: implement product selection and total discounted price calculation in CartPage

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -39,6 +39,11 @@ const CartPage = () => {
 		}
 	};
 
+	const totalDiscountedPrice = selectedProducts.reduce(
+		(acc, product) => acc + product.discountedTotal * 1000,
+		0
+	);
+
 	return (
 		<div>
 			<h1 className="text-2xl font-bold mb-4">Your Cart</h1>
@@ -46,95 +51,141 @@ const CartPage = () => {
 			{products.length === 0 ? (
 				<p>Your cart is empty</p>
 			) : (
-				<div className="w-full min-w-[45dvw] mx-auto">
-					<div className="flex justify-between mb-4 items-center">
-						<label htmlFor="selectAll" className="cursor-pointer">
-							<Checkbox
-								id="selectAll"
-								size="md"
-								color="success"
-								className="ml-4 max-h-8 mt-3"
-								onChange={(e) => {
-									handleSelectAll(e.target.checked);
-								}}
-								isSelected={selectedProducts.length === products.length}
-							/>
-							<span className="text-gray-200 text-base">
-								Select All ({products.length} products)
-							</span>
-						</label>
+				<div className="w-full grid grid-cols-[2fr_1fr] gap-4 min-w-[45dvw] mx-auto">
+					<div>
+						<div className="flex justify-between mb-4 items-center">
+							<label htmlFor="selectAll" className="cursor-pointer">
+								<Checkbox
+									id="selectAll"
+									size="md"
+									color="success"
+									className="ml-4 max-h-8 mt-3"
+									onChange={(e) => {
+										handleSelectAll(e.target.checked);
+									}}
+									isSelected={selectedProducts.length === products.length}
+								/>
+								<span className="text-gray-200 text-base">
+									Select All ({products.length} products)
+								</span>
+							</label>
 
-						{selectedProducts.length > 0 && (
-							<Button
-								variant="bordered"
-								color="danger"
-								size="sm"
-								className="mt-6 p-3 max-w-min border-none"
-								onPress={() => {
-									selectedProducts.forEach((product) => {
-										removeProduct(product.id);
-									});
-									setSelectedProducts([]);
-								}}
+							{selectedProducts.length > 0 && (
+								<Button
+									variant="bordered"
+									color="danger"
+									size="sm"
+									className="mt-6 p-3 max-w-min border-none"
+									onPress={() => {
+										selectedProducts.forEach((product) => {
+											removeProduct(product.id);
+										});
+										setSelectedProducts([]);
+									}}
+								>
+									<TrashIcon width={20} color="gray" /> Delete
+								</Button>
+							)}
+						</div>
+
+						{products.map((product) => (
+							<Card
+								radius="lg"
+								key={product.id}
+								className="mb-4 bg-current h-20"
+								fullWidth
 							>
-								<TrashIcon width={20} color="gray" /> Delete
-							</Button>
-						)}
-					</div>
+								<CardBody>
+									<div className="grid grid-cols-[1fr_8fr_1fr] gap-2 overflow-hidden">
+										<Checkbox
+											size="md"
+											color="success"
+											className="ml-4 max-h-8 mt-3"
+											onChange={() => handleSelectProduct(product.id)}
+											isSelected={selectedProducts.some(
+												(selectedProduct) => selectedProduct.id === product.id
+											)}
+										/>
 
-					{products.map((product) => (
-						<Card
-							radius="lg"
-							key={product.id}
-							className="mb-4 bg-current h-20"
-							fullWidth
-						>
-							<CardBody>
-								<div className="grid grid-cols-[1fr_8fr_1fr] gap-2 overflow-hidden">
-									<Checkbox
-										size="md"
-										color="success"
-										className="ml-4 max-h-8 mt-3"
-										onChange={() => handleSelectProduct(product.id)}
-										isSelected={selectedProducts.some(
-											(selectedProduct) => selectedProduct.id === product.id
-										)}
-									/>
+										<div className="-ml-4 w-full flex flex-col gap-1 text-gray-300">
+											<div className="grid grid-cols-[2fr_0.1fr] gap-2">
+												<span className="flex gap-2">
+													<Image
+														src={product.thumbnail}
+														alt={product.title}
+														className="w-full h-full object-cover rounded-lg"
+														width={60}
+													/>
+													<h2 className="text-base">{product.title}</h2>
+												</span>
+												<Button
+													variant="bordered"
+													color="danger"
+													size="sm"
+													className="mt-6 p-3 max-w-min border-none"
+												>
+													<TrashIcon width={20} color="gray" />
+												</Button>
+											</div>
+										</div>
 
-									<div className="-ml-4 w-full flex flex-col gap-1 text-gray-300">
-										<div className="grid grid-cols-[2fr_0.1fr] gap-2">
-											<span className="flex gap-2">
-												<Image
-													src={product.thumbnail}
-													alt={product.title}
-													className="w-full h-full object-cover rounded-lg"
-													width={60}
-												/>
-												<h2 className="text-base">{product.title}</h2>
-											</span>
-											<Button
-												variant="bordered"
-												color="danger"
-												size="sm"
-												className="mt-6 p-3 max-w-min border-none"
-											>
-												<TrashIcon width={20} color="gray" />
-											</Button>
+										<div className="flex flex-col items-end justify-center text-gray-200">
+											<p className="text-sm line-through text-gray-500">
+												{formatPriceIDR(product.total * 1000)}
+											</p>
+											<p className="text-lg font-semibold">
+												{formatPriceIDR(product.discountedTotal * 1000)}
+											</p>
 										</div>
 									</div>
+								</CardBody>
+							</Card>
+						))}
+					</div>
 
-									<div className="flex flex-col items-end justify-center text-gray-200">
-										<p className="text-sm line-through text-gray-500">
-											{formatPriceIDR(product.total * 1000)}
-										</p>
-										<p className="text-lg font-semibold">
-											{formatPriceIDR(product.discountedTotal * 1000)}
-										</p>
+					<Card className="bg-current">
+						<CardBody>
+							<div className="text-gray-200 px-4">
+								<h2 className="text-lg font-bold mb-2">Order Summary</h2>
+
+								<div className="flex justify-between py-3">
+									<span>Total</span>
+									<div className="grid">
+										{selectedProducts.length > 0 ? (
+											<p className="line-through text-gray-500 font-normal text-sm">
+												{formatPriceIDR(total * 1000)}
+											</p>
+										) : (
+											"-"
+										)}
+										{totalDiscountedPrice ? (
+											<p className="font-semibold">
+												{formatPriceIDR(
+													selectedProducts.reduce(
+														(acc, product) =>
+															acc + product.discountedTotal * 1000,
+														0
+													)
+												)}
+											</p>
+										) : null}
 									</div>
 								</div>
-							</CardBody>
-						</Card>
-					))}
+								<Button
+									variant="solid"
+									color="success"
+									fullWidth
+									className="mt-2 text-white font-semibold disabled:cursor-not-allowed"
+									isDisabled={selectedProducts.length === 0}
+									onPress={() => {
+										// Handle checkout logic here
+									}}
+								>
+									Checkout ({selectedProducts.length})
+								</Button>
+							</div>
+						</CardBody>
+					</Card>
 				</div>
 			)}
 		</div>
