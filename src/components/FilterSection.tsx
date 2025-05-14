14e8246433a3d0628f39a15f95ab89b8821a59a4
syahrilhanla <syahrilhanla5@gmail.com>
2025-05-14 23:44:56 +0700

    feat: clear filter and sort by button click

--- src/components/FilterSection.tsx
+++ src/components/FilterSection.tsx
@@ -4,7 +4,11 @@ import { useEffect, useState } from "react";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { useQuery } from "@tanstack/react-query";
 
-import { AdjustmentsHorizontalIcon, TagIcon } from "@heroicons/react/16/solid";
+import {
+	AdjustmentsHorizontalIcon,
+	TagIcon,
+	XMarkIcon,
+} from "@heroicons/react/16/solid";
 import { Select, SelectItem } from "@heroui/react";
 
 import pushQueryURL from "@/helpers/pushQueryURL";
@@ -15,6 +19,9 @@ import { useCategoryStore } from "@/store/category.store";
 const FilterSection = () => {
 	const [selectedSort, setSelectedSort] = useState<string | null>(null);
 	const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
+
+	const [openCategory, setOpenCategory] = useState(false);
+	const [openSort, setOpenSort] = useState(false);
 
 	const router = useRouter();
 	const pathname = usePathname();
@@ -48,21 +55,19 @@ const FilterSection = () => {
 
 	const categories = data as Category[];
 
-	const newURL = pushQueryURL(
-		{
-			sortBy: selectedSort ? selectedSort.split(" ")[0] : undefined,
-			order: selectedSort ? selectedSort.split(" ")[1] : undefined,
-			category: selectedCategory ? selectedCategory : undefined,
-		},
-		pathname,
-		searchParams.toString()
-	);
-
 	useEffect(() => {
-		if (newURL) {
-			router.push(newURL);
-		}
-	}, [newURL, router]);
+		const newURL = pushQueryURL(
+			{
+				sortBy: selectedSort ? selectedSort.split(" ")[0] : undefined,
+				order: selectedSort ? selectedSort.split(" ")[1] : undefined,
+				category: selectedCategory ? selectedCategory : undefined,
+			},
+			pathname,
+			searchParams.toString()
+		);
+
+		router.push(newURL);
+	}, [selectedCategory, selectedSort, router]);
 
 	return (
 		<div className="w-full flex flex-wrap gap-3 items-center mb-4">
@@ -73,14 +78,50 @@ const FilterSection = () => {
 				onSelectionChange={(sortBy) =>
 					setSelectedSort(sortBy.currentKey || null)
 				}
+				isOpen={openSort}
+				onOpenChange={(open) => {
+					setOpenSort(open);
+					if (open) {
+						setOpenCategory(false);
+					}
+				}}
 				radius="full"
 				variant="bordered"
+				value={selectedSort || ""}
 				classNames={{
 					trigger:
 						"bg-white shadow-sm border focus:shadow-md focus:border-none focus:ring-0 focus:outline-none",
 				}}
 				startContent={<AdjustmentsHorizontalIcon width={20} color="gray" />}
 				isDisabled={isLoading}
+				renderValue={(value) => {
+					return (
+						<div
+							className="flex items-center gap-2"
+							onMouseDown={(e) => {
+								e.preventDefault();
+								e.stopPropagation();
+							}}
+						>
+							<p className="text-gray-700">
+								{filters.find((filter) => filter.key == selectedSort)?.label ||
+									"Sort By"}
+							</p>
+							{selectedSort && (
+								<button
+									type="button"
+									onClick={(e) => {
+										setSelectedSort(null);
+										setOpenSort(false);
+									}}
+									className="-mt-4 ml-3 absolute right-8 z-10 hover:bg-red-300 rounded-full p-0.5 duration-200"
+								>
+									<XMarkIcon width={16} color="red" />
+								</button>
+							)}
+						</div>
+					);
+				}}
 			>
 				{filters.map((filter) => (
 					<SelectItem
@@ -97,9 +138,46 @@ const FilterSection = () => {
 				label="Category"
 				isDisabled={isLoading}
 				placeholder="Category"
+				isOpen={openCategory}
 				onSelectionChange={(categoryUrl) =>
 					setSelectedCategory(categoryUrl.currentKey || null)
 				}
+				onOpenChange={(open) => {
+					setOpenCategory(open);
+					if (open) {
+						setOpenSort(false);
+					}
+				}}
+				renderValue={(value) => {
+					return (
+						<div
+							className="flex items-center gap-2"
+							onMouseDown={(e) => {
+								e.preventDefault();
+								e.stopPropagation();
+							}}
+						>
+							<p className="text-gray-700">
+								{categories.find(
+									(category) => category.slug == selectedCategory
+								)?.name || "Category"}
+							</p>
+							{selectedCategory && (
+								<button
+									type="button"
+									onClick={(e) => {
+										setSelectedCategory(null);
+										setOpenCategory(false);
+									}}
+									className="-mt-4 ml-3 absolute right-8 z-10 hover:bg-red-300 rounded-full p-0.5 duration-200"
+								>
+									<XMarkIcon width={16} color="red" />
+								</button>
+							)}
+						</div>
+					);
+				}}
+				items={categories}
 				radius="full"
 				variant="bordered"
 				classNames={{
