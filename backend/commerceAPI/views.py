c0037749110bd16996058afb9681e28848d2c6dc
syahrilhanla <syahrilhanla5@gmail.com>
2025-05-19 13:25:27 +0700

    feat: add Django Filter support to views and settings

--- backend/commerceAPI/views.py
+++ backend/commerceAPI/views.py
@@ -1,13 +1,16 @@
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from .models import Product, Category, Order, Review, Cart, CartItem
 from .serializers import ProductSerializer, CategorySerializer, OrderSerializer, ReviewSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 
 # Create your views here.
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['title', 'description', 'category', 'tags']
+    filterset_fields = ['category', 'price', 'rating', 'stock', 'brand']
+    ordering_fields = ['created_at', 'updated_at', 'price', 'rating']
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
