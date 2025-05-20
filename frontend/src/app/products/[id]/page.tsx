7865272d485d5f9ffbf898e9af56ab3c777120eb
syahrilhanla <syahrilhanla5@gmail.com>
2025-05-20 11:30:11 +0700

    feat: integrate product detail and review APIs

--- frontend/src/app/products/[id]/page.tsx
+++ frontend/src/app/products/[id]/page.tsx
@@ -4,10 +4,11 @@ import { useQuery } from "@tanstack/react-query";
 import { use } from "react";
 
 import ProductMainInfo from "@/components/ProductDetail/ProductMainInfo";
-import ProductPreview from "@/components/ProductDetail/ProductPreview";
 import ProductProcess from "@/components/ProductDetail/ProductProcess";
 import ProductReview from "@/components/ProductDetail/ProductReview";
 import ProductDetailSkeleton from "@/components/ProductDetailSkeleton";
+import ProductPreview from "@/components/ProductDetail/ProductPreview";
+
 import { Product } from "@/types/Product.type";
 
 interface Props {
@@ -20,21 +21,36 @@ const ProductDetail = ({ params }: Props) => {
 	// Unwrap the params object using React's `use` hook
 	const { id } = use(params);
 
-	const { data, isLoading, isError } = useQuery({
+	const {
+		data: product,
+		isLoading,
+		isError,
+	} = useQuery({
 		queryKey: ["product", id],
 		queryFn: async () => {
-			const response = await fetch(`https://dummyjson.com/products/${id}`);
-			if (!response.ok) {
+			const productResponse = await fetch(
+				`http://localhost:8000/api/products/${id}`
+			);
+			const reviewResponse = await fetch(
+				`http://localhost:8000/api/reviews/?product_id=${id}`
+			);
+
+			if (!productResponse.ok || !reviewResponse.ok) {
 				throw new Error("Failed to fetch product");
 			}
-			return response.json();
+
+			// return {}
+			return {
+				...(await productResponse.json()),
+				reviews: await reviewResponse.json(),
+			} as Product;
 		},
 	});
 
 	if (isLoading) return <ProductDetailSkeleton />;
 	if (isError) return <div>Error loading product</div>;
 
-	const product = data as Product;
+	console.log(product);
 
 	return (
 		<>
