b6678d4d4afc14eeed69452a0573f4904a8e9417
syahrilhanla <syahrilhanla5@gmail.com>
2025-05-21 10:16:59 +0700

    feat: add user info store with Zustand for state management

---
+++ frontend/src/store/userInfo.store.ts
@@ -0,0 +1,24 @@
+import { create } from "zustand";
+
+type UserInfo = {
+  id: string;
+  name: string;
+  email: string;
+  userName: string;
+};
+
+interface UserInfoState {
+  userInfo: UserInfo | null;
+  setUserInfo: (userInfo: UserInfo | null) => void;
+  clearUserInfo: () => void;
+  accessToken: string | null;
+  setAccessToken: (accessToken: string | null) => void;
+}
+
+export const useUserInfoStore = create<UserInfoState>((set) => ({
+  userInfo: null,
+  setUserInfo: (userInfo) => set({ userInfo }),
+  clearUserInfo: () => set({ userInfo: null }),
+  accessToken: null,
+  setAccessToken: (accessToken) => set({ accessToken }),
+}));
