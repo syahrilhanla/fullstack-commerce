e43f01caa18325060d8aecf02892bb37ce0abff4
syahrilhanla <syahrilhanla5@gmail.com>
2025-06-02 18:54:56 +0700

    feat: show core infos in order card

--- frontend/src/app/orders/page.tsx
+++ frontend/src/app/orders/page.tsx
@@ -1,14 +1,18 @@
 "use client";
 
 import { useFetchQuery } from "@/helpers/dataQuery";
-import { formatDate } from "@/helpers/helpers";
-import { useUserInfoStore } from "@/store/userInfo.store";
+import { formatDate, formatPriceIDR } from "@/helpers/helpers";
 import { Order } from "@/types/Order.type";
-import { Chip, Card, CardHeader } from "@heroui/react";
+import {
+	Chip,
+	Card,
+	CardHeader,
+	CardBody,
+	Image,
+	Divider,
+} from "@heroui/react";
 
 const OrderListPage = () => {
-	const { userInfo } = useUserInfoStore();
-
 	const { data, isError, isLoading } = useFetchQuery(
 		"http://localhost:8000/api/orders/user_orders/",
 		["orders"]
@@ -20,10 +24,13 @@ const OrderListPage = () => {
 			{isLoading && <p className="text-gray-500">Loading transactions...</p>}
 			{isError && <p className="text-red-500">Failed to load transactions.</p>}
 
-			{data && data.length > 0 && (
+			{data?.orders && data.orders.length > 0 && (
 				<ul className="grid gap-4">
-					{data.map((order: Order) => (
-						<Card className="min-w-80 max-w-3xl mx-auto border-none shadow-lg">
+					{data.orders.map((order: Order) => (
+						<Card
+							key={order.id}
+							className="min-w-80 max-w-3xl mx-auto border-none shadow-lg"
+						>
 							<CardHeader className="flex gap-2">
 								<p className="text-xs text-gray-700">
 									{formatDate(order.createdAt)}
@@ -31,6 +38,43 @@ const OrderListPage = () => {
 								<StatusChip status={order.orderStatus} />
 								<p className="text-xs text-gray-400">{order.externalId}</p>
 							</CardHeader>
+							<CardBody>
+								<div className="grid grid-cols-[2fr_7fr_1fr] gap-2">
+									<Image
+										src={order.orderItems[0].product.thumbnail}
+										height={60}
+										width={60}
+										alt={order.orderItems[0].product.title}
+										className="object-cover rounded-lg w-24 h-24"
+										loading="lazy"
+									/>
+
+									<div className="grid gap-1">
+										<p className="text-lg font-semibold text-gray-800">
+											{order.orderItems[0].product.title}
+										</p>
+										<p>
+											{order.orderItems[0].quantity} items x{" "}
+											{formatPriceIDR(order.orderItems[0].price)}
+										</p>
+										{order.orderItems.length > 1 && (
+											<p>
+												{order.orderItems.length > 1
+													? `and ${order.orderItems.length - 1} more items`
+													: ""}
+											</p>
+										)}
+									</div>
+
+									<div className="flex gap-0">
+										<Divider orientation="vertical" className="hr-10 mr-1" />
+
+										<p className="text-lg font-bold text-gray-800">
+											{formatPriceIDR(order.totalPrice)}
+										</p>
+									</div>
+								</div>
+							</CardBody>
 						</Card>
 					))}
 				</ul>
