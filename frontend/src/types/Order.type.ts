f4f84a891772ee1b57566a64e35d13bab40ab889
syahrilhanla <syahrilhanla5@gmail.com>
2025-06-02 18:49:44 +0700

    feat: add orderItems to Order interface for enhanced order structure

--- frontend/src/types/Order.type.ts
+++ frontend/src/types/Order.type.ts
@@ -1,6 +1,20 @@
+import { Product } from "./Product.type";
+
+export interface OrderItem {
+  id: number;
+  quantity: number;
+  price: number;
+  createdAt: string;
+  updatedAt: string;
+  order: number;
+  product: Product;
+  user: number;
+}
+
 export interface Order {
   id: number;
   externalId: string | null;
+  orderItems: OrderItem[];
   totalPrice: number;
   orderStatus: "pending" | "paid" | string;
   createdAt: string;
