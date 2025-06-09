5021db95c70e63ecdf7c69b9785c7ae9630e118a
syahrilhanla <syahrilhanla5@gmail.com>
2025-06-09 21:52:41 +0700

    refactor: adjust ProductSkeleton size and layout

--- frontend/src/components/ProductCatalogue/ProductSkeleton.tsx
+++ frontend/src/components/ProductCatalogue/ProductSkeleton.tsx
@@ -1,25 +1,24 @@
 import { Card, Skeleton } from "@heroui/react";
 
 const ProductSkeleton = () => {
-	return [1, 2, 3, 4, 5].map((i) => (
+	return Array.from({ length: 8 }).map((_, i) => (
 		<Card
 			key={i}
-			className="w-[200px] space-y-5 p-4 bg-white border border-gray-200 shadow-md hover:shadow-lg transition-shadow duration-200"
+			className="pt-2 bg-white w-full max-w-xs min-w-0 cursor-pointer border border-gray-200 shadow-sm flex flex-col"
 			radius="lg"
+			shadow="lg"
 		>
-			<Skeleton className="rounded-lg bg-gray-200 animate-pulse">
-				<div className="h-24 rounded-lg bg-gray-200" />
-			</Skeleton>
-			<div className="space-y-3">
-				<Skeleton className="w-3/5 rounded-lg bg-gray-200 animate-pulse">
-					<div className="h-3 w-3/5 rounded-lg bg-gray-200" />
-				</Skeleton>
-				<Skeleton className="w-4/5 rounded-lg bg-gray-200 animate-pulse">
-					<div className="h-3 w-4/5 rounded-lg bg-gray-200" />
-				</Skeleton>
-				<Skeleton className="w-2/5 rounded-lg bg-gray-200 animate-pulse">
-					<div className="h-3 w-2/5 rounded-lg bg-gray-200" />
-				</Skeleton>
+			<div className="pb-0 gap-1 pt-2 px-4 flex-col items-start">
+				<Skeleton className="h-4 w-24 mb-2 rounded bg-gray-200 animate-pulse" />
+				<Skeleton className="h-3 w-16 mb-1 rounded bg-gray-200 animate-pulse" />
+				<Skeleton className="h-5 w-32 rounded bg-gray-200 animate-pulse" />
+			</div>
+			<div className="overflow-visible py-2 flex-1 flex items-center justify-center">
+				<Skeleton className="object-contain rounded-xl w-[100px] h-[100px] sm:w-[120px] sm:h-[120px] mx-auto bg-gray-200 animate-pulse" />
+			</div>
+			<div className="flex flex-col gap-0 text-left justify-start w-full px-4 pb-3">
+				<Skeleton className="h-5 w-24 mb-1 rounded bg-gray-200 animate-pulse" />
+				<Skeleton className="h-4 w-16 rounded bg-gray-200 animate-pulse" />
 			</div>
 		</Card>
 	));
